e53bcdacda11f072089aa2ff33737203106d8dea
monstrasitix <injectedinferno@gmail.com>
2016-10-30 12:28:07 +0000

    Landing Page

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,10 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     var vm = this;
     vm.authentication = Authentication;
-  }
-]);
+
+    vm.icons = [
+      { title: 'Statistics', icon: 'multiline_chart', content: 'Some random rambling content' },
+      { title: 'Open Source API', icon: 'cloud_download', content: 'Some random rambling content' },
+      { title: 'Flexible Storage', icon: 'storage', content: 'Some random rambling content' }
+    ];
+  }]);
